06027ef8f81242dd40162130a097602df24c4ba5
Matheus Bittencourt <matheusgithub341@gmail.com>
2022-06-20 18:38:07 -0300

    First commit

---
+++ Velocidade permitida/Exercicio01.js
@@ -0,0 +1,7 @@
+// var vel = velocidade
+var vel = 60.1
+console.log('A velocidade do seu carro é ' + vel + ' Km/h')
+if (vel > 60) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
+console.log('Dirija sempre usando cinto de segurança!')
